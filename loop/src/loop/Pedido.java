2cea87f6c8070fd72577b48a1064a6da33cd15f1
Joao Henrique <jhifmachado@gmail.com>
2024-03-10 18:50:09 -0300

    commit teste

---
+++ loop/src/loop/Pedido.java
@@ -0,0 +1,26 @@
+package loop;
+
+
+public class Pedido {
+    private int codigoP;
+    private String dataPedido;
+    Usuario user = new Usuario();
+
+    public int getCodigoP() {
+        return codigoP;
+    }
+
+    public void setCodigoP(int codigoP) {
+        this.codigoP = codigoP;
+    }
+
+    public String getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(String dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+    
+    
+}
